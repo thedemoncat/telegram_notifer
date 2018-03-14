a17151e32fbb6a137b361e118e1200b0756963b8
ZhdanovR <ruslan.zhdanov@omskcarbon.com>
2018-03-14 16:42:35 +0600

    Добавил вывод имени компьютера

--- service_monitor.cmd
+++ service_monitor.cmd
@@ -4,13 +4,13 @@ SetLocal EnableExtensions
 set application=%1
 set token=%2
 set chat_id=%3
-set textMessege=Service drop %1
+set textMessege=Server %COMPUTERNAME%. Service drop %1
 
 tasklist | find /I "%application%" >nul
 
 if errorlevel 1 (
   
-  %SystemRoot%\System32\WindowsPowerShell\v1.0\PowerShell.exe -ExecutionPolicy ByPass -command ". './telegram_notifer.ps1' '%token%' '%chat_id%' '%textMessege%'"
+  %SystemRoot%\System32\WindowsPowerShell\v1.0\PowerShell.exe -ExecutionPolicy ByPass -command ". '%~dp0/telegram_notifer.ps1' '%token%' '%chat_id%' '%textMessege%'"
 goto exit
 ) else (
   echo %application% is running
